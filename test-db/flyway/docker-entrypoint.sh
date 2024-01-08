0ae606b9d9460b1655e1a82bfc9a243dd97e9d9e
jimmy.lee <dongeol92@daum.net>
2024-01-08 23:59:06 +0900

    schema

---
+++ test-db/flyway/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "wait DB container up"
+dockerize -wait tcp://db-mysql:3306 -timeout 60s
+
+# 스키마 마이그레이션
+echo "#####################################################"
+echo "########### run database schema migration ###########"
+echo "#####################################################"
+flyway -configFiles=/flyway/conf/flyway.conf -locations=filesystem:/flyway/sql/schema migrate
+
+# Seed 데이터 마이그레이션
+echo "########################################"
+echo "########### insert seed data ########### "
+echo "########################################"
+flyway -configFiles=/flyway/conf/flyway.conf -locations=filesystem:/flyway/sql/seed migrate
