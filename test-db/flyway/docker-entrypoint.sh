ebd5543d5158436e630646929ecf179f0c2dd8d0
donggeol <80632039+e-build@users.noreply.github.com>
2024-01-23 12:35:47 +0900

    Update docker-entrypoint.sh
    entrypoint print pretty

--- test-db/flyway/docker-entrypoint.sh
+++ test-db/flyway/docker-entrypoint.sh
@@ -3,12 +3,14 @@ echo "wait DB container up"
 dockerize -wait tcp://db-mysql:3306 -timeout 60s
 
 # 스키마 마이그레이션
+echo
 echo "#####################################################"
 echo "########### run database schema migration ###########"
 echo "#####################################################"
 flyway -configFiles=/flyway/conf/flyway.conf -locations=filesystem:/flyway/sql/schema migrate
 
 # Seed 데이터 마이그레이션
+echo
 echo "########################################"
 echo "########### insert seed data ########### "
 echo "########################################"
